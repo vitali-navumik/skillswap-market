dff4e260e6702628428f1855c89d71a0a08dcd5b
Vitali <vitali@lorexgrupp.ee>
2026-04-20 20:28:41 +0300

    Extend offer creation security tests

--- tests-framework/src/test/java/offer/OfferCreationTests.java
+++ tests-framework/src/test/java/offer/OfferCreationTests.java
@@ -1,22 +1,35 @@
 package offer;
 
+import com.vitali.framework.CommonAssertions;
 import com.vitali.framework.api.offer.assertions.OfferAssertions;
+import com.vitali.framework.api.offer.OfferActions;
 import com.vitali.framework.api.offer.requests.CreateOfferRequest;
 import com.vitali.framework.api.offer.responses.OfferResponse;
+import com.vitali.framework.api.users.providers.UserRoleProvider.RoleNotAllowedToCreateOffer;
+import com.vitali.framework.connectors.ConnectorResponse;
+import com.vitali.framework.connectors.RestAssuredConnector;
+import com.vitali.framework.connectors.Sender;
 import com.vitali.framework.enums.UserPreset;
 import com.vitali.framework.resolvers.ActionsContainer;
 import com.vitali.framework.resolvers.GlobalActionsParameterResolver;
 import com.vitali.framework.resolvers.GlobalActionsPreset;
+import com.vitali.framework.resolvers.UserCreationHelper;
 import com.vitali.framework.tags.OfferTag;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @OfferTag
 @ExtendWith({GlobalActionsParameterResolver.class})
 class OfferCreationTests {
+
+    private final OfferActions guestOfferActions = new OfferActions(new Sender(null, new RestAssuredConnector()));
 
     @Test
     @DisplayName("Mentor can create offer")
@@ -50,5 +63,36 @@ class OfferCreationTests {
         OfferAssertions.checkOfferDataIsCorrect(secondOffer, new OfferAssertions.AssertionParams()
                 .id(firstOffer.getId())
                 .publicId(firstOffer.getPublicId()));
+    }
+
+    @ParameterizedTest
+    @EnumSource(RoleNotAllowedToCreateOffer.class)
+    @DisplayName("User without mentor role cannot create offer")
+    void userWithoutMentorRoleCannotCreateOffer(RoleNotAllowedToCreateOffer role) {
+        ActionsContainer actor = resolveUserActions(role);
+        CreateOfferRequest offerRequest = CreateOfferRequest.builder().build();
+
+        ConnectorResponse<OfferResponse> response = actor.offerActions().createOffer(offerRequest);
+
+        CommonAssertions.checkForbidden(response);
+        assertThat(response.getDataResponse())
+                .as("Only mentor can create an offer")
+                .contains("MENTOR role is required to create an offer");
+    }
+
+    @Test
+    @DisplayName("Guest cannot create offer without token")
+    void guestCannotCreateOfferWithoutToken() {
+        CreateOfferRequest offerRequest = CreateOfferRequest.builder().build();
+
+        ConnectorResponse<OfferResponse> response = guestOfferActions.createOffer(offerRequest);
+
+        CommonAssertions.checkForbidden(response);
+    }
+
+    private ActionsContainer resolveUserActions(RoleNotAllowedToCreateOffer role) {
+        return switch (role) {
+            case STUDENT, ADMIN -> UserCreationHelper.createUserAndLogIn(UserPreset.valueOf(role.name()));
+        };
     }
 }
